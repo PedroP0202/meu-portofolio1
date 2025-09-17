5ce381f0122dcb7b40ad7b8a11472efb726f7cdb
Pedro Piedade <160158774+PedroP0202@users.noreply.github.com>
2025-09-17 20:51:39 +0100

    add LAB2
    foi add o começo do novo exercício

---
+++ Meu Portofólio/menu.js
@@ -0,0 +1,34 @@
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+btnMenu.addEventListener('click', ()=> {
+
+    menu.classList.add('menu-mobile-show');
+   
+});
+
+menu.addEventListener('click', ()=> {
+    menu.classList.remove('menu-mobile-show');
+});
+overlay.addEventListener('click', ()=> {
+    menu.classList.remove('menu-mobile-show');
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const targetId = this.getAttribute('href').substring(1); // Pega o ID do link
+      const targetElement = document.getElementById(targetId);
+  
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'  // Isso garante que o scroll seja suave
+      });
+    });
+  });
+  
+  setTimeout(() => {
+    window.scrollTo({top: targetElement.offsetTop, behavior: "smooth"});
+}, 100); // Adiciona um pequeno delay de 100ms
